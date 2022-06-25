015999067ff2b94387c737e153d239507d484f15
Fahim Shahriar <74281428+fahimShahriar6832@users.noreply.github.com>
2022-06-25 17:29:51 +0600

    Update image_predictor.py

--- image_predictor.py
+++ image_predictor.py
@@ -4,7 +4,7 @@ st.header("Image Predictor")
 
 html_temp = """
 <div style="background-color:#025246 ;padding:10px">
-<h2 style="color:white;text-align:center;">Try again and again!!!</h2>
+<h2 style="color:white;text-align:center;background-color: Blue;">Try again and again!!!</h2>
 <h3 style="color:red;text-align:center;">You will crack it Fahim</h3>
 </div>
 """
