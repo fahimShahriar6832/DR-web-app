3548e22e3810b734e6af019750efdf0813553904
Fahim Shahriar <74281428+fahimShahriar6832@users.noreply.github.com>
2022-06-29 22:40:36 +0600

    Update image_predictor.py

--- image_predictor.py
+++ image_predictor.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from PIL import Image
 from keras.models import load_model
-from keras.preprocessing import image
-import tensorflow as tf
-import keras
 
 st.header("Image Predictor")
 
@@ -15,21 +12,15 @@ html_temp = """
 """
 st.markdown(html_temp, unsafe_allow_html=True)
 
+
 uploaded_file = st.file_uploader("Upload a fundas image")
 
-
-
-test_image = keras.utils.load_img(uploaded_file, target_size = (224,224))
-test_image = keras.utils.load_img(test_image)
-test_image = test_image/255
-test_image = np.expand_dims(test_image, axis = 0)
-result = Fundus_covid19.predict(test_image)
-
-im = Image.open(uploaded_file)
-im = im.resize((224,224))
-im = np.array(im)
-im = im/255
-im = np.expand_dims(im,axis=0)
+if uploaded_file is not None:
+    im = Image.open(uploaded_file)
+    im = im.resize((224,224))
+    im = np.array(im)
+    im = im/255
+    im = np.expand_dims(im,axis=0)
 
 # load model
 Fundus_covid19 = load_model("DR_VGG19_new.h5")
@@ -40,6 +31,8 @@ if result[0][0] > result[0][1]:
     print("Diabetic Retinopathy [{:.2f}% accuracy]".format((result[0][0]*100)))
 else:
   print("NO Diabetic Retinopathy [{:.2f}% accuracy]".format((result[0][1])*100))
+
+
 
 
 
